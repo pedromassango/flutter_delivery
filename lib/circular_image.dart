58c6cf17cb7510006717afa7a70eb6284e84e7cb
pedromassango <pedromassango.developer@gmail.com>
2019-01-08 23:35:58 +0100

    created custom circular image

---
+++ lib/circular_image.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CircularImage extends StatelessWidget {
+  final double _width, _height;
+  final ImageProvider image;
+
+  CircularImage(this.image, {double width = 40, double height = 40})
+      : _width = width,
+        _height = height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: _width,
+      height: _height,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        image: DecorationImage(image: image),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.black45,
+          )
+        ]
+      ),
+    );
+  }
+}
